a39cdc4b980f7e284aec058423c0786286014a8d
blackberry-web <maryananakonechna123@gmail.com>
2025-01-26 22:38:01 +0200

    feat: add types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,16 @@
-export type Card = {
-    
+export type DisplayDays = {
+    prevMonth: Date[], 
+    currentMonth: Date[], 
+    nextMonth: Date[]
 }
 
 export type Task = {
-    
-}
+    id: string,
+    day: Date | string | undefined,
+    task: string,
+} | null;
+
+export type Holiday = {
+    date: Date | string,
+    name: string,
+};
